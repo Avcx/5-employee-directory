f54acb1c792e6d47bafd15ffe21447b4c41130e3
Tonio Adams <avcx12@gmail.com>
2023-04-28 17:53:46 -0600

    Basic functionality completed

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,41 @@
     This is the javascript code for this employee directory application.
 
 */
-console.log('helloo')
+
+const profilesPerPage = 12
 
 async function fetchData(url) {
    return await fetch(url)
     .then(res => res.json())
+    .then(data => parseUsers(data.results))
     .catch(err => console.log(err));
 }
 
-let users = fetchData('https://randomuser.me/api/?results=12');
-console.log(users);
+fetchData('https://randomuser.me/api/?results=12');
+
+let employees;
+
+function parseUsers(users) {
+    employees = users;
+    createUsers();
+}
+
+
+function createUsers() {
+    const gallery = document.querySelector('#gallery')
+    employees.forEach(employee => {
+        console.log(employee);
+        let employeeCard = 
+        `   <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${employee.picture.large}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <p class="card-text">${employee.email}</p>
+                    <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+                </div>
+            </div> `
+        gallery.insertAdjacentHTML('beforeend', employeeCard);
+    })
+}
