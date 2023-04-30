f287a0d8f07a28ac0a01647a8013657994fb91ea
Tonio Adams <avcx12@gmail.com>
2023-04-30 13:07:11 -0600

    Fixed bugs with search feature

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,17 @@
 
 const profilesPerPage = 12;
 const profilesToLoad = 12;
+
 let currentPage = 1;
+let currentList = [];
+let employees;
+
 const pageButtons = document.querySelector('.page-links');
 const gallery = document.querySelector('#gallery');
 const searchContainer = document.querySelector('.search-container');
+const searchCounter = document.querySelector('#search-counter');
+
+
 let modalContainer;
 
 searchContainer.insertAdjacentHTML('beforeend', `
@@ -30,13 +37,15 @@ function search(e) {
     const searchFor = searchBox.value.toLowerCase();
 
     if (/\w+/ig.test(searchFor)) {
-        searchResults = employees.filter( employee => employee.name.first.toLowerCase().includes(searchFor) || employee.name.last.toLowerCase().includes(searchFor))
+        searchResults = employees.filter( employee => employee.name.first.toLowerCase().includes(searchFor) || employee.name.last.toLowerCase().includes(searchFor));
+        currentList = searchResults;
         showPage(searchResults, 1);
+        searchCounter.textContent = `There were ${searchResults.length} results found for "${searchFor}"`
     } else {
+        currentList = employees;
         showPage(employees, 1);
+        searchCounter.textContent = ``;
     }
-    if (searchFor )
-    gallery.insertAdjacentHTML('afterbegin', `<h4>There were ${searchResults.length} results found for "${searchFor}"</h4>`);
 }
 
 /*
@@ -48,10 +57,15 @@ fetch(`https://randomuser.me/api/?results=${profilesToLoad}&nat=us,ca`)
     .then(data => parseUsers(data.results))
     .catch(err => console.log(err));
 
-let employees;
+function parseUsers(data) {
+    let users = [];
+    for (i=0; i < data.length; i++) {
+        data[i].id = i;
+        users.push(data[i])
+    }
 
-function parseUsers(users) {
     employees = (users);
+    currentList = employees;
     showPage(employees, 1);
 }
 
@@ -97,13 +111,14 @@ function showPage(list, page) {
         }
     }
 
-    function createEmployee(employee, id) {
+    function createEmployee(employee, itemNumber) {
 
 
         const employeeCard = document.createElement('div');
 
         employeeCard.setAttribute('class', 'card');
-        employeeCard.setAttribute('id', id);
+        employeeCard.setAttribute('id', employee.id);
+        employeeCard.setAttribute('data-item-number', itemNumber);
 
         // Add relevant employee info to the 'employeeCard' element
 
@@ -114,7 +129,7 @@ function showPage(list, page) {
 
         // Adds an event listener to the 'employeeCard' element to open the modal
 
-        employeeCard.addEventListener('click', event => {
+        employeeCard.addEventListener('click', _e => {
                 showModal(employeeCard);
          });
     };
@@ -164,88 +179,95 @@ function showPage(list, page) {
 // }
 
 function showModal(input) {
+    let htmlReference
     let employeeNum;
+    let cardItemNumber;
 
-    // This ternary statment checks to see if an element or an id number was passed through.
+    if (input.id) {
+        htmlReference = input;
+    } else {
+        htmlReference = gallery.children[input];
+    }
 
-    input.id ? employeeNum = parseInt(input.id) : employeeNum = parseInt(input);
+    employeeNum = parseInt(htmlReference.id)
+    cardItemNumber = parseInt(htmlReference.getAttribute('data-item-number'));
 
     // This if statement 
 
-    if (employeeNum === undefined) return false;
+    if (employeeNum === undefined) {return false};
 
-        let employeePhone;
-        const phoneNumberRegEx = /([A-Z0-9]{3})[\D]*([A-Z0-9]{3})[\D]*([A-Z0-9]{4})[\D]*/i;
-        let employee = employees[employeeNum];
-        let employeeBirthday = new Date(employee.dob.date)
+    let employee = employees[employeeNum];
+    let employeePhone;
+    const phoneNumberRegEx = /([A-Z0-9]{3})[\D]*([A-Z0-9]{3})[\D]*([A-Z0-9]{4})[\D]*/i;
+    let employeeBirthday = new Date(employee.dob.date)
 
-        if (phoneNumberRegEx.test(employee.cell)) {
-            if (!employee.cell.startsWith('(')) {
-                employeePhone = employee.cell.replace(phoneNumberRegEx, "($1) $2-$3");
-            } else {
-                employeePhone = employee.cell;
-            }
+    if (phoneNumberRegEx.test(employee.cell)) {
+        if (!employee.cell.startsWith('(')) {
+            employeePhone = employee.cell.replace(phoneNumberRegEx, "($1) $2-$3");
+        } else {
+            employeePhone = employee.cell;
         }
+    }
 
-        modalContainer = document.createElement('div');
-        modalContainer.className = 'modal-container';
-        gallery.insertAdjacentElement('afterend', modalContainer)
+    modalContainer = document.createElement('div');
+    modalContainer.className = 'modal-container';
+    gallery.insertAdjacentElement('afterend', modalContainer)
 
-        // modal variable holds all the mark up for the modal window
+    // modal variable holds all the mark up for the modal window
 
-        let modal = `            
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-                    <p class="modal-text">${employee.email}</p>
-                    <p class="modal-text cap">${employee.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${employeePhone}</p>
-                    <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${employeeBirthday.getDate()}/${employeeBirthday.getMonth() + 1}/${employeeBirthday.getFullYear()}</p>
-                </div>
+    let modal = `            
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="modal-text">${employee.email}</p>
+                <p class="modal-text cap">${employee.location.city}</p>
+                <hr>
+                <p class="modal-text">${employeePhone}</p>
+                <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                <p class="modal-text">Birthday: ${employeeBirthday.getDate()}/${employeeBirthday.getMonth() + 1}/${employeeBirthday.getFullYear()}</p>
             </div>
-            <div class="modal-btn-container">`;
+        </div>
+        <div class="modal-btn-container">`;
 
-        // These conditional statements append `Next` or `Prev` buttons as needed.
-        
-        if (employeeNum !== 0) {
-            modal += '<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>'
+    // These conditional statements append `Next` or `Prev` buttons as needed.
+    
+    if (cardItemNumber !== 0) {
+        modal += '<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>'
+    }
+    if (cardItemNumber !== currentList.length - 1) {
+        modal += '<button type="button" id="modal-next" class="modal-next btn">Next</button>'
+    }
+
+    modal += `
+    </div>`;
+
+    modalContainer.insertAdjacentHTML('afterbegin', modal);
+
+    function scrollUsers(direction) {
+
+        if (direction === 'Next') {
+            showModal(cardItemNumber + 1);
+        } else {
+            showModal(cardItemNumber - 1);
         }
-        if (employeeNum !== employees.length - 1) {
-            modal += '<button type="button" id="modal-next" class="modal-next btn">Next</button>'
-        }
+    }
 
-        modal += `
-        </div>`;
+    document.querySelector('.modal-container').addEventListener('click', event => {
 
-        modalContainer.insertAdjacentHTML('afterbegin', modal);
+        if (event.target.tagName === 'BUTTON' || event.target.tagName === 'STRONG') {
 
-        function scrollUsers(direction) {
+            if (event.target.id === 'modal-close-btn' || event.target.parentNode.id === 'modal-close-btn') {
+                modalContainer.remove();
 
-            if (direction === 'Next') {
-                showModal(employeeNum + 1);
             } else {
-                showModal(employeeNum - 1);
+                let action = event.target.textContent;
+                modalContainer.remove();
+                scrollUsers(action);
             }
         }
-
-        document.querySelector('.modal-container').addEventListener('click', event => {
-
-            if (event.target.tagName === 'BUTTON' || event.target.tagName === 'STRONG') {
-
-                if (event.target.id === 'modal-close-btn' || event.target.parentNode.id === 'modal-close-btn') {
-                    modalContainer.remove();
-
-                } else {
-                    let action = event.target.textContent;
-                    modalContainer.remove();
-                    scrollUsers(action);
-                }
-            }
-        })
+    })
 }
 
 // pageButtons.addEventListener('click', (e) => {
